b1a92aef03a03f7b598a4040be41c2d422da8fbb
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-03-02 12:16:33 +0000

    fix: fix indentation

--- Python/year.py
+++ Python/year.py
@@ -1,17 +1,11 @@
-y = int(input('Enter a year ='))
-    if (y % 4 == 0)
-  :
-  if (y % 100 == 0)
-    :
-    if (y % 400 == 0)
-      :
-      print('it is a leap year')
-      else
-        :
-        print('It is not a leap year')
-        else
-          :
-          print('It is a leap year')
-          else
-            :
-            print('It is not a leap year')
+y = int(input("Enter a year ="))
+if y % 4 == 0:
+    if y % 100 == 0:
+        if y % 400 == 0:
+            print("it is a leap year")
+        else:
+            print("It is not a leap year")
+    else:
+        print("It is a leap year")
+else:
+    print("It is not a leap year")
